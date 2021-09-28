b4870efaa48a9b03d38c35527e41eed6e98ccae0
Gabriela Bergamo <gabiibergamo@gmail.com>
2021-09-28 10:37:30 -0300

    linkagem das páginas

--- js/main.js
+++ js/main.js
@@ -1,22 +1,25 @@
-let inputnome = document.querySelector('input')
+window.addEventListener('load', () => {
+    let inputnome = document.querySelector('input')
 
-let dadosform = document.querySelectorAll('input')
-console.log(dadosform)
+    let dadosform = document.querySelectorAll('input')
+    console.log(dadosform)
 
-let tituloprincipal = document.querySelector('h1')
-tituloprincipal.innerText = "Faça parte do nosso time"
+    let tituloprincipal = document.querySelector('h1')
+    tituloprincipal.innerText = "Faça parte do nosso time"
 
-let inputemail = document.querySelector('#email')
-inputemail.setAttribute('placeholder', 'jane@mail.com')
+    let inputemail = document.querySelector('#email')
+    inputemail.setAttribute('placeholder', 'jane@mail.com')
 
-let inputcargahoraria = document.querySelector('#carga-horaria')
-console.log(inputcargahoraria.innerHTML)
+    let inputcargahoraria = document.querySelector('#carga-horaria')
+    console.log(inputcargahoraria.innerHTML)
 
-inputcargahoraria.innerHTML += '<option value="Trainee">Trainee</option>'
+    inputcargahoraria.innerHTML += '<option value="Trainee">Trainee</option>'
 
-tituloprincipal.style.color = 'orange'
+    tituloprincipal.style.color = 'orange'
 
-let elementoDiv = document.querySelector('div')
+    let elementoDiv = document.querySelector('div')
 
-//elementoDiv.classList.add("novaClasse")
-//elementoDiv.classList.remove("container")
+    //elementoDiv.classList.add("novaClasse")
+    //elementoDiv.classList.remove("container")
+})
+
